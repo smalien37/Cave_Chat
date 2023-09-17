3f7c0a98a0bcff01e66957aee5d24e02cf4919c7
smalien37 <smalien37@gmail.com>
2023-09-17 19:18:08 +0530

    Pushing to the dev BRANCH

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-def abcd(nums):
-    m,c = 1, 1
-    nums.sort()
-    for i in range(1,len(nums)):
-        if nums[i] - nums[i-1] == 1:
-            c += 1
-        else:
-            m = max(m,c)
-            c = 1
-    m = max(m,c)
-    return m
+def maxPoints(points) -> int:
+    m,mi = {},-1e9
+    for i in range(len(points) - 1):
+        for j in range(i+1,len(points)):
+            if (points[j][0] - points[i][0]) == 0:
+                r = points[j][0]
+            else: r =  (points[j][1] - points[i][1]) / (points[j][0] - points[i][0])
+            m[r] = 1 + m.get(r,0)
+    
+    for j in m:
+        mi = max(mi,m[j])
 
-print(abcd([0,3,7,2,5,8,4,6,0,1]))
+    print(m)
+    return mi
+
+print(maxPoints([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]))
